e94718c8876994f7e37d5c84362d25f737fda3c3
Phillip Longman <himself@philliplongman.com>
2017-06-29 23:37:55 -0500

    Hello :name.

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -1,8 +1,11 @@
 defmodule HelloPhoenix.HelloController do
   use HelloPhoenix.Web, :controller
 
+  def world(conn, %{"name" => name}) do
+    render conn, "world.html", name: name
+  end
   def world(conn, _params) do
-    render conn, "world.html"
+    render conn, "world.html", name: "world"
   end
 
 end
